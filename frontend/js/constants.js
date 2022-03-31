03a0ca5077d09abb918c58f3df97f237d8f821b3
Adrian <102465968+TheCryptures@users.noreply.github.com>
2022-03-31 20:11:47 +0200

    Commit on main

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -3,7 +3,7 @@ const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the Cryptures NFT Project!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The Cryptures NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
+const welcome_p = 'The Cryptures NFT Project is a decentralized, open-source project that aims to let you earn with your NFTs, provide proper knowledge fallowed by energy saving. Follow step by step on the <a href="https://www.youtube.com/watch?v=6GykJwo-82k&t=5s&ab_channel=ForestGrandfatherStudio"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
